e72e71718ed07ffd4edc145fe9cb08748e8da41f
David McMackins <mcmackinsd@gmail.com>
2014-05-02 09:41:10 -0500

    Implemented functional program

---
+++ src/MainWindow.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author 2mac
+ */
+public class MainWindow extends JFrame
+{
+    public MainWindow()
+    {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setName("iDad");
+        setTitle("iDad");
+        setLayout(new GridLayout(2,1,5,5));
+        setSize(300,200);
+
+        final JTextField text = new JTextField("");
+        text.setEditable(false);
+
+        JButton next = new JButton("Next Message");
+        next.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent)
+            {
+                text.setText(getJoke());
+            }
+        });
+
+        add(text);
+        add(next);
+
+        setVisible(true);
+    }
+
+    public int getRandom(int bottom, int top)
+    {
+        Random dice = new Random();
+        int dif = top - bottom;
+        return bottom + dice.nextInt(dif+1);
+    }
+
+    public String getJoke()
+    {
+        ArrayList<String> jokes = new ArrayList<String>();
+        jokes.add("Hi, user. My name is dad.");
+
+        return jokes.get(getRandom(0,jokes.size()-1));
+    }
+
+    public static void main(String[] args)
+    {
+        new MainWindow();
+    }
+}
